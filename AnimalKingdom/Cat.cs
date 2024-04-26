938b1768d3cfb52f76e7da0cced062eb6a9f88a4
DiogoMeiraFreire <didi9934@gmail.com>
2024-04-26 10:46:02 +0100

    Added Animal, Dog and Cat

---
+++ AnimalKingdom/Cat.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+    Author:     Steven Hall 
+    contact:    stevenbryanb.work@gmail.com
+
+    Comply with the terms of the MPL 2.0!
+ */
+
+namespace AnimalKingdom {
+    /// <summary>
+    /// Represents a very basic cat
+    /// </summary>
+    public class Cat : Animal, IMammal
+    {//                    implementação 
+
+        //
+        public int NumberOfNipples { get; } // gets property
+                    
+        // ------------------------------------------------------------------ //
+
+        /// <summary>
+        /// Defines a cat, initializes with 6 nipples
+        /// Inherits from Animal and implements IMammal
+        /// </summary>
+        public Cat()
+        {
+
+            NumberOfNipples = 6; // set number of nipples to 6
+            
+        }
+
+        /// <summary>
+        /// Overrides sound adding base + miau
+        /// </summary>
+        /// <returns></returns>
+        public override string Sound() // overrides sound
+        {
+            return $"{base.Sound()} Miau";
+        }
+    }
+}
+// -----------------------------------------------------------------------------
---
+++ AnimalKingdom/Dog.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+    Author:     Steven Hall 
+    Contact:    stevenbryanb.work@gmail.com
+
+    Comply with the terms of the MPL 2.0!
+ */
+
+namespace AnimalKingdom {
+
+    /// <summary>
+    /// Represents a basic Dog with 8 nipples.
+    /// Inherits from Animal and implements IMammal
+    /// </summary>
+    public class Dog : Animal, IMammal
+    {
+        // Number of nipples this animal
+        // Gets & sets with constructor
+        public int NumberOfNipples { get; }          // gets property
+
+        // ------------------------------------------------------------------ //
+
+        /// <summary>
+        /// Initializes a new instance of the Dog class.
+        /// Inherits from Animal and implements IMammal
+        /// </summary>
+        public Dog()
+        {   
+            // initializes dog with 8 nipples
+            NumberOfNipples = 8;
+        }
+
+        /// <summary>
+        /// Overrides the Sound method from Animal with a Dog sound.
+        /// </summary>
+        /// <returns>
+        /// Base string + "Woof!" in string format.
+        /// </returns>
+        public override string Sound()
+        {
+            return $"{base.Sound()} Woof!";
+        }
+    }
+}
+// -----------------------------------------------------------------------------
