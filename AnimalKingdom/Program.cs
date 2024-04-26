9a55d54772e54182c7d47a6f05a6836378547a2b
DiogoMeiraFreire <didi9934@gmail.com>
2024-04-26 10:42:41 +0100

    Added AnimalKingdom

---
+++ AnimalKingdom/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AnimalKingdom
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
