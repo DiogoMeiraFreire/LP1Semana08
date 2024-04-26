147c3766df13f3b2a09b2d64f78b1a8341378fb5
DiogoMeiraFreire <didi9934@gmail.com>
2024-04-26 13:15:47 +0100

    Fix some AnimalKingdom to use the ones available in the moodle

--- AnimalKingdom/Animal.cs
+++ AnimalKingdom/Animal.cs
@@ -1,21 +1,10 @@
-﻿/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-    Author: Steven Hall 
-    contact stevenbryanb.work@gmail.com
-
-    Comply with the terms of the MPL 2.0!
- */
-
-namespace AnimalKingdom
+﻿namespace AnimalKingdom
 {
     public abstract class Animal
     {
         public virtual string Sound()
         {
-            return "Sound will be";
+            return "Sound will be ";
         }
     }
 }
